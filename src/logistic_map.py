83fc27c0a8f1502283e0637a5b58d298eaef3f32
John Allen Ringer <johnjack0987@gmail.com>
2023-02-05 15:17:29 -0700

    Update logistic_map.py

--- src/logistic_map.py
+++ src/logistic_map.py
@@ -1,16 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def gen_log_map(n: int, r: float, x0: float = 0.5) -> np.array:
+def gen_log_map(n: int, r: float, y0: float = 0.5) -> np.array:
     """
-    Generate array containing values for logisitic map
+    Generate array containing values for logistic map
     :param n: int, number of points
     :param r: float, rate
-    :param x0: float, initial value
-    :return: numpy array containing values
+    :param y0: float, initial value
+    :return: x, y where x is an array of time-vals and y is the population-vals
     """
     x = np.zeros(n)
-    x[0] = x0
+    y = np.zeros(n)
+    x[0], y[0] = 0, y0
     for i in range(n - 1):
-        x[i + 1] = r * x[i] * (1 - x[i])
-    return x
+        y[i + 1] = r * y[i] * (1 - y[i])
+        x[i + 1] = i + 1
+    return x, y
+
+
+def main():
+    # create plot of logistic map
+    r = 4.0
+    n = 100
+    x0 = 0.8
+    x, y = gen_log_map(n, r, x0)
+    plt.plot(x, y)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
