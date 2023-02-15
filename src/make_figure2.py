d15590c3d31b4da51af3e4952e1f7081225e4e9f
John Allen Ringer <johnjack0987@gmail.com>
2023-02-14 18:41:49 -0700

    Update make_figure2.py to use multiple runs/config sources

--- src/make_figure2.py
+++ src/make_figure2.py
@@ -6,9 +6,19 @@ from matplotlib import style
 import numpy as np
 
 
-def main(dfs: list):
-    # expects stats from 3 config files
-    assert len(dfs) == 3
+def add_stats(dfs: list, ax, first_plot=True):
+    colors1 = ['purple', 'pink']
+    labels1 = ['Infected Cells', 'Virions']
+    for df in dfs:
+        df['tot_infected'] = df['incb'] + df['expr']
+        plot_subplot(ax, df, ['tot_infected', 'virs'], labels=labels1,
+                     colors=colors1, title='', first_plot=first_plot)
+        first_plot = False
+    return None
+
+
+def main(dfs1: list, dfs2: list, dfs3: list):
+    # expects stats from 3 config files, each having multiple runs
     style.use('fivethirtyeight')
 
     plt.rc('axes', titlesize=12)
@@ -17,31 +27,33 @@ def main(dfs: list):
     plt.rc('ytick', labelsize=8)
     plt.rc('legend', fontsize=8)
     plt.rcParams['figure.dpi'] = 200
-    fig, axs = plt.subplots(1, len(dfs), figsize=(8, 4),
-                            constrained_layout=True)
-    colors1 = ['purple', 'pink']
-    labels1 = ['Infected Cells', 'Virions']
+    fig, axs = plt.subplots(1, 3, figsize=(8, 4),
+                            constrained_layout=True, sharey='row')
+    dfs = [dfs1, dfs2, dfs3]
+    titles = ['a', 'b', 'c']
     for idx, ax in enumerate(axs):
-        df = dfs[idx]
-        df['tot_infected'] = df['incb'] + df['expr']
-        ax.set_ylabel("Count")
-        plot_subplot(ax, df, ['tot_infected', 'virs'], labels=labels1,
-                     colors=colors1, title='')
-
+        add_stats(dfs[idx], ax)
+        ax.set_title(titles[idx])
+    axs[0].set_ylabel("Count")
+    axs[1].set_xlabel("Time (Days)")
     plt.show()
     return fig
 
 
 def plot_subplot(ax, df, columns, title, colors, labels, alpha=1.0,
-                 log_scale=True):
+                 log_scale=True, first_plot=True):
     xs = df['time'].map(lambda x: x / 1440)  # convert from mins -> days
     for i, col in enumerate(columns):
         ys = df[col]
         if log_scale:
             ys = ys.map(lambda y: y + 1)  # include 0-vals
-        ax.plot(xs, ys, label=labels[i], lw=2, alpha=alpha, color=colors[i])
-    ax.legend(loc='upper left')
-    ax.set_xlabel('Time (days)')
+        if first_plot:
+            ax.plot(xs, ys, label=labels[i], lw=1, alpha=alpha, color=colors[i])
+        else:
+            ax.plot(xs, ys, lw=1, alpha=alpha, color=colors[i])
+    if not first_plot:
+        return None
+    ax.legend(loc='lower right')
     ax.set_title(title)
     xticks = ax.get_xticks()
     if xticks[1] - xticks[0] > 1 and len(xs) > 0:
@@ -53,10 +65,22 @@ def plot_subplot(ax, df, columns, title, colors, labels, alpha=1.0,
     return None
 
 
+def get_dfs(data_dir: str):
+    dfs = []
+    for run_dir in os.listdir(data_dir):
+        fpath = os.path.join(data_dir, run_dir, "simcov.stats")
+        stats_df = pd.read_csv(fpath, sep='\t')
+        stats_df = stats_df.rename(columns={'# time': 'time'})
+        dfs.append(stats_df)
+    return dfs
+
+
 if __name__:
-    fpath = "../data/simcov/stats_files/simcov.stats"
-    assert os.path.exists(fpath)
-    stats_df = pd.read_csv(fpath, sep='\t')
-    stats_df = stats_df.rename(columns={'# time': 'time'})
-    # will use 3 configs once we have all of them
-    main([stats_df, stats_df, stats_df])
+    # will use different data dirs once we have all our configs
+    data_dir1 = "../data/simcov/stable"
+    data_dir2 = "../data/simcov/stable"
+    data_dir3 = "../data/simcov/stable"
+    dfs_1 = get_dfs(data_dir1)
+    dfs_2 = get_dfs(data_dir2)
+    dfs_3 = get_dfs(data_dir3)
+    main(dfs_1, dfs_2, dfs_3)
