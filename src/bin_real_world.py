5f545c2adb5e9c4e1b36395b0bd57bf0cfbe67e0
James Morris <james.morris0102@gmail.com>
2023-02-19 09:51:50 -0700

    doin stuff

--- src/bin_real_world.py
+++ src/bin_real_world.py
@@ -6,9 +6,10 @@ Script used to bin real-world data from:
 https://github.com/owid/covid-19-data/tree/master/public/data/jhu
 """
 import pandas as pd
+import numpy as np
 
 
-def main(cases_pth: str, deaths_pth: str):
+def main(cases_pth: str, deaths_pth: str, save_path: str):
     """
     Bin covid world data.
     :param cases_pth: str, path to new cases data
@@ -17,13 +18,22 @@ def main(cases_pth: str, deaths_pth: str):
     """
     cases_df = pd.read_csv(cases_pth)
     deaths_df = pd.read_csv(deaths_pth)
-    world_cases_df = cases_df.rename(columns={'World': 'Cases'})['Cases']
+    
+    #world_cases_df = cases_df.rename(columns={'World': 'Cases'})['Cases']
     world_deaths_df = deaths_df.rename(columns={'World': 'Deaths'})['Deaths']
-    df = pd.DataFrame([world_cases_df, world_deaths_df]).T
-    print(df.head())
+    df = pd.DataFrame([world_deaths_df]).T
+    df_bin = df
+    n_bins = 15
+    labels = np.arange(n_bins)
+    #df_bin['Cases'] = pd.cut(df['Cases'], labels=labels,
+     #                               bins=n_bins)
+    df_bin['Deaths'] = pd.cut(df['Deaths'], labels=labels, bins=n_bins)
+    df.to_csv(save_path, header=None, index=None, sep=' ', mode='a')
 
 
 if __name__ == "__main__":
     cases_path = "../data/real_world/new_cases.csv"
     deaths_path = "../data/real_world/new_deaths.csv"
-    main(cases_path, deaths_path)
+    sv_path = "../data/real_world/binned_deaths.txt"
+    main(cases_path, deaths_path, sv_path)
+    
---
+++ src/make_figure4.py
@@ -0,0 +1,87 @@
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib import style
+import numpy as np
+
+
+def add_stats(df: pd.DataFrame, axs, first_plot=True):
+    ax_casesvdeaths = axs
+    ax_casesvdeaths.set_ylabel('Count')
+    colors1 = ['red', 'black']
+    labels1 = ['Cases', 'Deaths']
+
+
+    plot_subplot(ax_casesvdeaths, df, ['Cases', 'Deaths'], labels=labels1, colors=colors1,
+                 title='', first_plot=first_plot)
+    return None
+
+
+def plot_subplot(ax, df, columns, title, colors, labels, alpha=1.0,
+                 log_scale=True, first_plot=True):
+    xs = df['day']  # convert from mins -> days
+    for i, col in enumerate(columns):
+        ys = df[col]
+        if log_scale:
+            ys = ys.map(lambda y: y + 1)  # include 0-vals
+        if first_plot:
+            ax.plot(xs, ys, label=labels[i], lw=1, alpha=alpha, color=colors[i])
+        else:
+            ax.plot(xs, ys, lw=1, alpha=alpha, color=colors[i])
+    if not first_plot:
+        return None
+    ax.legend(loc='upper left')
+    ax.set_xlabel('Time (days)')
+    ax.set_title(title)
+    xticks = ax.get_xticks()
+    if log_scale:
+        ax.set_yscale('log')
+        max_val = df[columns].max(axis=1)
+        ax.set_ylim(0.5, 10 * np.max(max_val))
+    return None
+
+
+def main(data_dir: str, save_pth: str = None):
+    assert os.path.exists(data_dir)
+    style.use('fivethirtyeight')
+    plt.rc('axes', titlesize=12)
+    plt.rc('axes', labelsize=10)
+    plt.rc('xtick', labelsize=8)
+    plt.rc('ytick', labelsize=8)
+    plt.rc('legend', fontsize=8)
+    plt.rcParams['figure.dpi'] = 200
+    fig, axs = plt.subplots(1, 1, figsize=(8, 4), constrained_layout=True)
+    first_plot = True
+
+    fpath = os.path.join(data_dir, "new_cases.csv")
+    fpath1 = os.path.join(data_dir, "new_deaths.csv")
+    stats_df = pd.read_csv(fpath)
+    stats_df1 = pd.read_csv(fpath1)
+
+    df = pd.DataFrame(stats_df["World"])
+
+    df1 = pd.DataFrame(stats_df1["World"])
+
+    cases_df = df.rename(columns={'World': 'Cases'})['Cases']
+    deaths_df = df1.rename(columns={'World': 'Deaths'})['Deaths']
+
+    newdf = pd.DataFrame([cases_df, deaths_df]).T
+
+    newdf.insert(0,"day",np.arange(0,len(newdf)))
+    print(newdf.head())
+
+
+    add_stats(newdf, axs, first_plot)
+    first_plot = False  # don't add additional keys, not necessary\
+
+    if save_pth is not None:
+        plt.savefig(save_pth)
+    plt.show()
+    return fig
+
+
+if __name__:
+    data_folder = "../data/real_world"
+    save_pth = "../data/real_world/figures/figure4.png"
+    main(data_folder, save_pth)
